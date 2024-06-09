fb63d65b9b95a783693c541e686d07ffafa64300
MartynaBawor <163142946+MartynaBawor@users.noreply.github.com>
2024-06-09 22:52:41 +0200

    commit all

---
+++ apbd_9/Services/IDbService.cs
@@ -0,0 +1,18 @@
+
+using apbd_9.Models;
+
+namespace apbd_9.Services;
+
+public interface IDbService
+{
+    Task<ICollection<Trip>> GetTripsSorted(int page, int pageSize);
+    Task<int> GetTotalTripsCount();
+    Task<Client?> GetClientById(int idClient);
+    Task DeleteClient(Client client);
+    Task<bool> ClientHasTrips(int idClient);
+    Task<Client?> GetClientByPesel(string clientDtoPesel);
+    Task<bool> ClientIsAssignedToTrip(string clientDtoPesel, int clientDtoIdTrip);
+    Task<Trip?> GetTripById(int clientDtoIdTrip);
+    Task AddClient(Client client);
+    Task AddClientTrip(ClientTrip clientTrip);
+}
